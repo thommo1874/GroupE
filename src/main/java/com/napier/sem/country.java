5c362a805817007968706e02cb2c1aa4d33e9500
carolynethomson <thommo1874@gmail.com>
2021-03-12 00:17:23 +0000

    Updating Master with new DB connection

---
+++ src/main/java/com/napier/sem/country.java
@@ -0,0 +1,66 @@
+package com.napier.sem;
+/**
+ * Represents a country
+ */
+public class country {
+    /**
+     * Code
+     */
+    public String Code;
+    /**
+     * Country name
+     */
+    public String Name;
+    /**
+     * Continent
+     */
+    public String Continent;
+    /**
+     * Region
+     */
+    public String Region;
+    /**
+     * Surface Area
+     */
+    public double SurfaceArea;
+    /**
+     * Independence Year
+     */
+    public int IndepYear;
+    /**
+     * Population
+     */
+    public int Population;
+    /**
+     * Life Expectancy
+     */
+    public double LifeExpectancy;
+    /**
+     * GNP
+     */
+    public double GNP;
+    /**
+     * GNP Old
+     */
+    public double GNPOld;
+    /**
+     * Local name
+     */
+    public String LocalName;
+    /**
+     * Government Form
+     */
+    public String GovernmentForm;
+    /**
+     * Head of State
+     */
+    public String HeadOfState;
+    /**
+     * Capital
+     */
+    public int Capital;
+    /**
+     * Code2
+     */
+    public String Code2;
+}
