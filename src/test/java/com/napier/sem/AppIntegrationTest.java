14fa00c49f3a517ae65f3676cc69c4036a3b7a51
carolynethomson <thommo1874@gmail.com>
2021-03-23 17:10:20 +0000

    Adding testing fixes

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -14,7 +14,7 @@ public class AppIntegrationTest {
     @BeforeAll
     static void init() {
         app = new App();
-        app.connect("localhost:33060");
+        app.connect("database:3306");
     }
 
     @Test
